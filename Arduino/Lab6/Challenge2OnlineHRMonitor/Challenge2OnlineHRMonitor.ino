46c4d5c5d1339953a440b8ab0491840c9bbb7e65
tajo0215 <43757648+tajo0215@users.noreply.github.com>
2021-05-15 23:11:36 -0700

    Adding Challenge 2 Lab 6 files

---
+++ Arduino/Lab6/Challenge2OnlineHRMonitor/Challenge2OnlineHRMonitor.ino
@@ -0,0 +1,41 @@
+int ax = 0; int ay = 0; int az = 0;
+int ppg = 0;
+int sampleTime = 0;
+bool sending;
+
+void setup() {
+  setupAccelSensor();
+  setupCommunication();
+  setupDisplay();
+  setupPhotoSensor();
+  sending = false;
+  writeDisplay("Sleep", 0, true);
+}
+
+void loop() {
+
+  String command = receiveMessage();
+
+  if(command == "sleep") {
+    sending = false;
+    writeDisplay("Sleep", 0, true);
+  }
+
+  else if(command == "wearable") {
+    sending = true;
+    writeDisplay("Wearable", 0, true);
+  }
+
+  else if (command.substring(0,2) == "HR") {
+    String hr_count = command.substring(2);
+    String hr = "HR: " + hr_count;
+    writeDisplay(hr.c_str(), 1, true);
+  }
+
+  if (sending && sampleSensors()) {
+    String response = String(sampleTime) + ",";
+    response += String(ax) + "," + String(ay) + "," + String(az);
+    response += "," + String(ppg) + "\n";
+    sendMessage(response);
+  }
+}
---
+++ Python/Lab6/challenge2_online_hr_monitor.py
@@ -0,0 +1,69 @@
+"""
+Lab 6 Challenge 2: Online Heart Rate Monitor
+@author: Jun Park (A15745118)
+"""
+
+from ECE16Lib.Communication import Communication
+#from ECE16Lib.CircularList import CircularList
+from ECE16Lib.HRMonitor import HRMonitor
+from matplotlib import pyplot as plt
+from time import sleep
+from time import time
+from sys import exit
+#import numpy as np
+
+if __name__ == "__main__":
+    fs = 50
+    num_samples = 500
+    process_time = 1
+    
+    hr_monitor = HRMonitor(num_samples, fs, [])
+    
+    comms = Communication("COM4", 115200)
+    comms.clear()
+    input("Ready to collect data? Press [ENTER] to begin.\n")
+    print("Start measuring in...")
+    for k in range(3,0,-1):
+        print(k)
+        sleep(1)
+    print("Begin!")
+    
+    comms.send_message("wearable")
+    
+    try:
+        previous_time = time()
+        
+        while(True):
+            message = comms.receive_message()
+            if(message != None):
+                try:
+                    (m1, _, _, _, m2) = message.split(",")
+                except Exception as e:        # if corrupted data, skip the sample
+                    print(e)
+                    print("Corrupted data. Skipping sample: {}".format(message))
+                    continue
+                
+                hr_monitor.add(int(m1)/1e3, int(m2))
+                
+                current_time = time()
+                if (current_time - previous_time > process_time):
+                    previous_time = current_time
+                    
+                    hr, peaks, filtered = hr_monitor.process()
+                    hr_msg = "HR{:f}".format(hr)
+                    print(hr_msg)
+                    comms.send_message(hr_msg)
+                    
+                    plt.cla()
+                    plt.plot(filtered)
+                    plt.title("Heart Rate: %f bpm" % hr)
+                    plt.show(block=False)
+                    plt.pause(0.001)
+    except(KeyboardInterrupt) as e:
+        print(e)
+    finally:
+        print("Closing connection.")
+        comms.send_message("sleep")  # stop sending data
+        sleep(3)
+        comms.close()
+        exit()
